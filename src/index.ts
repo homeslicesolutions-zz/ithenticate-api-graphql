ed7d800b96086f3d28c9a33860f98b0c76fec402
Joe Vu <jvu@chegg.com>
2020-10-08 11:52:29 -0700

    Fix port and added procfile

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 
 const app = express();
-const port = 7000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`App listening on ${PORT}`);
 });
